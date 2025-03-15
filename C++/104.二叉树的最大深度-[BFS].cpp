35774228399d7fc1a9d3a7b3f3fe6e82ebef655c
stephenqiu <Popcornqhd@gmail.com>
2025-03-15 20:20:13 +0800

    feat:添加每日一题系列[[104] 二叉树的最大深度

---
+++ C++/104.二叉树的最大深度-[BFS].cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=cpp
+ *
+ * [104] 二叉树的最大深度 [BFS]
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        queue<TreeNode *> q;
+        q.push(root);
+        int depth = 0;
+        while (!q.empty())
+        {
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+};
+// @lc code=end
