5e7846d2164a4f9d55a08759b2b84f379474921b
stephenqiu <Popcornqhd@gmail.com>
2025-04-29 23:46:39 +0800

    feat:添加每日一题系列[[144] 二叉树的前序遍历]

---
+++ C++/四月版/144.二叉树的前序遍历-[DFS].cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=cpp
+ *
+ * [144] 二叉树的前序遍历 [DFS]
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        preorder(root, result);
+        return result;
+    }
+
+private:
+    void preorder(TreeNode *node, vector<int> &result)
+    {
+        if (node == nullptr)
+        {
+            return;
+        }
+        result.push_back(node->val);
+        preorder(node->left, result);
+        preorder(node->right, result);
+    }
+};
+// @lc code=end
