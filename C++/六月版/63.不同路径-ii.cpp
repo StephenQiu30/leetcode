23210bb7995c29ca53c8ea47e48d0728cd173e45
stephenqiu <Popcornqhd@gmail.com>
2025-06-10 23:40:42 +0800

    feat:添加每日一题系列[[63] 不同路径 II]

---
+++ C++/六月版/63.不同路径-ii.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=63 lang=cpp
+ *
+ * [63] 不同路径 II
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<int> dp(n, 0);
+        dp[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                {
+                    dp[j] = 0;
+                }
+                else if (j > 0)
+                {
+                    dp[j] += dp[j - 1];
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+};
+// @lc code=end
