ef300bdc8ab4ebd5ef69b96b67d7fadc8ae93801
Stephenqhd30 <Popcornqhd@gmail.com>
2025-07-31 23:44:05 +0800

    feat:添加每日一题系列[[219] 存在重复元素 II]

---
+++ C++/七月版/219.存在重复元素 II-[哈希表].cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=cpp
+ *
+ * [219] 存在重复元素 II [哈希表]
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (map.count(nums[i]) && i - map[nums[i]] <= k)
+            {
+                return true;
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
+// @lc code=end
