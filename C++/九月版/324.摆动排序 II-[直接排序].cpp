af3147277d44ad1e8c2c7b784f3896bbc570a717
stephenqiu <Popcornqhd@gmail.com>
2025-09-26 23:57:43 +0800

    feat:添加每日一题系列[[324] 摆动排序 II]

---
+++ C++/九月版/324.摆动排序 II-[直接排序].cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=324 lang=cpp
+ *
+ * [324] 摆动排序 II [直接排序]
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void wiggleSort(vector<int> &nums)
+    {
+        int n = nums.size();
+        if (n <= 1)
+        {
+            return;
+        }
+        vector<int> a = nums;
+        sort(nums.begin(), nums.end());
+        vector<int> res(n);
+        int mid = (n + 1) / 2;
+        int left = mid - 1;
+        int right = n - 1;
+        for (int i = 0; i < n; i++)
+        {
+            if ((i & 1) == 0)
+            {
+                // 偶数位置放左半段的最大剩余
+                res[i] = a[left--];
+            }
+            else
+            {
+                // 奇数位置放右半段的最大剩余
+                res[i] = a[right--];
+            }
+        }
+        nums.swap(res);
+    }
+};
+// @lc code=end
